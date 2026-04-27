a765d6a7b1463da06bc28386b0429c0699e30519
aetxabao <aitor.etxabarren@gmail.com>
2026-04-27 22:41:10 +0200

    cambiar escenas

---
+++ src/main/java/edu/masanz/da/juegored/controller/HostConfigController.java
@@ -0,0 +1,54 @@
+package edu.masanz.da.juegored.controller;
+
+import edu.masanz.da.juegored.model.UserSession;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+
+public class HostConfigController {
+
+    @FXML
+    private ComboBox<?> cmbLevel;
+
+    @FXML
+    private ImageView imvRoom;
+
+    @FXML
+    private RadioButton rbtPrivate;
+
+    @FXML
+    private RadioButton rbtPublic;
+
+    @FXML
+    private Spinner<?> spnNumero;
+
+    @FXML
+    private ToggleGroup tipo;
+
+    @FXML
+    private TextField txtSala;
+
+    @FXML
+    public void initialize() {
+        System.out.println("HostConfigController.initialize()");
+        String nickname = UserSession.getInstance().getNickname();
+        System.out.println("Nickname: " + nickname);
+    }
+
+    @FXML
+    void cancelarHostConfig(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearSala(ActionEvent event) {
+
+    }
+
+}
+
--- src/main/java/edu/masanz/da/juegored/controller/LauncherController.java
+++ src/main/java/edu/masanz/da/juegored/controller/LauncherController.java
@@ -1,17 +1,43 @@
 package edu.masanz.da.juegored.controller;
 
+import edu.masanz.da.juegored.model.UserSession;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LauncherController {
 
     @FXML
-    private TextField txtNick;
+    private TextField txtNickname;
 
     @FXML
     void crearSala(ActionEvent event) {
 
+        String nickname = txtNickname.getText();
+        System.out.println("LauncherController");
+        System.out.println("Nickname: " + nickname);
+        UserSession.getInstance().setNickname(nickname);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/masanz/da/juegored/hostconfig.fxml"));
+
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
