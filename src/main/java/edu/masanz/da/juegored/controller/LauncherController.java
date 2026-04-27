49b53318b82544a8e068f2d5b169db17b0f3f4c7
aetxabao <aitor.etxabarren@mariaanasanz.es>
2026-04-27 14:03:56 +0200

    ini

---
+++ src/main/java/edu/masanz/da/juegored/controller/LauncherController.java
@@ -0,0 +1,31 @@
+package edu.masanz.da.juegored.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LauncherController {
+
+    @FXML
+    private TextField txtNick;
+
+    @FXML
+    void crearSala(ActionEvent event) {
+
+    }
+
+    @FXML
+    void jugarSolo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void unirSala(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salir(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module edu.masanz.da.juegored {
+        requires javafx.controls;
+        requires javafx.fxml;
+
+        opens edu.masanz.da.juegored to javafx.fxml;
+        opens edu.masanz.da.juegored.controller to javafx.fxml;
+
+        exports edu.masanz.da.juegored;
+        exports edu.masanz.da.juegored.controller;
+}
